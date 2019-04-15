f29413f075f9efbc58a599f7bfd257b9d7015ff4
ken <kkang@presagia.com>
2019-04-14 22:45:48 -0400

    save source

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: 'build/'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader'
+                }),
+                test: /\.css$/
+            },
+            {
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { limit: 40000 }
+                    },
+                    'image-webpack-loader'
+                ],
+                test: /\.(jpe?g|png|gif|svg)$/
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
+
+// {
+//     use: ['style-loader', 'css-loader'],
+//     test: /\.css$/
+// }
+
+module.exports = config;
